8166b63768b5dc468d1bd2b576d241fa720d9d47
seerat zahra <seeratz4321@gmail.com>
2026-07-07 00:21:02 +0500

    Add feature importance visualization for Random Forest

---
+++ src/feature_importance.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+class FeatureImportance:
+    def plot(self, model, X_train, save_path):
+        if not hasattr(model, "feature_importance_"):
+            print("This model does not support feature importance.")
+            return
+        importance=pd.DataFrame(
+            {
+                "Feature":X_train.columns, 
+                "Importance":model.feature_importances_
+            }
+        )
+        importance=importance.sort_values(
+            by="Importance",
+            ascending=False
+        )
+        print("\nFEEature Importance\n")
+        print(importance)
+        plt.figure(figsize=(8,6))
+        plt.barh(
+            importance["Feature"],
+            importance["Importance"]
+        )        
+        plt.xlabel("Importance")
+        plt.ylabel("Feature")
+        plt.title("Feature Importance")
+        plt.gca().invert_yaxis()
+        plt.tight_layout()
+        plt.savefig(save_path)
+        plt.close()
